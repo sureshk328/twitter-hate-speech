5375c7ff3b2f7047f03f886c102677410b1be41f
Suresh <39266643+sureshk328@users.noreply.github.com>
2022-07-28 15:20:30 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ except ImportError:
     from collections import Mapping
 
 app = Flask(__name__)
-model = pickle.load(open('model_dt_clf.pkl', 'rb'))
-pickle.load(open('model_dt_clf.pkl','rb'))
+model_logreg = pickle.load(open('model_logreg.pkl', 'rb'))
+model_knn = pickle.load(open('model_knn.pkl', 'rb'))
+model_svm = pickle.load(open('model_svm.pkl', 'rb'))
+model_dectree = pickle.load(open('model_dectree.pkl', 'rb'))
 cv = pickle.load(open('transform.pkl','rb'))
 
 @app.route('/')
@@ -24,12 +26,18 @@ def predict():
 
     to_predict_list = list(to_predict_list.values())
     data = cv.transform([str(to_predict_list)]).toarray()
-    prediction = model.predict(data)
+    prediction_logreg = "Logistic Regression: "+model_logreg.predict(data)
+    prediction_knn = "KNN: "+model_knn.predict(data)
+    prediction_svm = "Support Vector Machine: "+model_svm.predict(data)
+    prediction_dectree = "DecisionTreeClassifier: "+ model_dectree.predict(data)
 
 
 
 
-    return render_template('index.html', prediction_text='Predicted Speech Type {}'.format(prediction))
+
+
+    #return render_template('index.html', prediction_text='Predicted Speech Type {}'.format(prediction))
+    return render_template('index.html',prediction_text="Below are prediction results: ", prediction_text_logreg=prediction_logreg, prediction_text_knn=prediction_knn,prediction_text_svn=prediction_svm, prediction_text_dectree=prediction_dectree)
 
 
 if __name__ == "__main__":
